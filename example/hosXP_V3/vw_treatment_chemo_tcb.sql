900372b68962b9ff3c66b47f634f58c2daea8e6d
Anon Thamcharoen <anonnon3952@gmail.com>
2025-05-01 12:10:10 +0700

    SQL QUERY HOSXPV3

---
+++ example/hosXP_V3/vw_treatment_chemo_tcb.sql
@@ -0,0 +1,24 @@
+DROP VIEW vw_treatment_chemo_tcb;
+
+CREATE VIEW vw_treatment_chemo_tcb AS
+SELECT * 
+FROM (
+SELECT CONCAT(SUBSTRING(p.cid, 1, 1), '-', SUBSTRING(p.cid, 2, 4), '-', SUBSTRING(p.cid, 6, 5), '-', SUBSTRING(p.cid, 11, 2), '-', SUBSTRING(p.cid, 13, 1)) AS cid, 
+        DATE_FORMAT(v.vstdate, "%Y%m%d") AS visit_date,
+        '3' AS treatment_code,
+        DATE_FORMAT(v.vstdate, "%Y%m%d") AS treatment_start_date,
+        od.icd10 AS icd10_code
+FROM vn_stat v
+LEFT OUTER JOIN ovstdiag od ON od.vn=v.vn
+LEFT OUTER JOIN patient p ON p.hn = v.hn
+WHERE v.vstdate = CURDATE()
+and `od`.`diagtype` = '1' 
+and ucase(`od`.`icd10`) like 'C%' 
+AND (v.op0 IN ("9925", "9928", "9929")
+OR v.op1 IN ("9925", "9928", "9929")
+OR v.op2 IN ("9925", "9928", "9929")
+OR v.op3 IN ("9925", "9928", "9929")
+OR v.op4 IN ("9925", "9928", "9929")
+OR v.op5 IN ("9925", "9928", "9929"))
+) DAT 
+WHERE icd10_code IS NOT NULL;
---
+++ example/hosXP_V3/vw_treatment_radiation_tcb.sql
@@ -0,0 +1,27 @@
+DROP VIEW vw_treatment_radiation_tcb;
+
+CREATE VIEW vw_treatment_radiation_tcb AS
+SELECT * 
+FROM (
+    SELECT 
+        CONCAT(SUBSTRING(p.cid, 1, 1), '-', SUBSTRING(p.cid, 2, 4), '-', SUBSTRING(p.cid, 6, 5), '-', SUBSTRING(p.cid, 11, 2), '-', SUBSTRING(p.cid, 13, 1)) AS cid, 
+        DATE_FORMAT(o.vstdate, "%Y%m%d") AS visit_date,
+        '2' AS treatment_code,
+        DATE_FORMAT(pcr.patient_cancer_first_diag_cancer_date, "%Y%m%d") AS treatment_start_date,
+        (select upper(replace(od.icd10, '.', ''))  
+          from ovstdiag od 
+          where od.vn = o.vn
+          and od.diagtype = '1' 
+          and upper(od.icd10) LIKE 'C%') AS icd10_code
+    FROM 
+        ovst o
+				LEFT OUTER JOIN ovstdiag od ON o.vn = od.vn
+        LEFT OUTER JOIN patient_cancer_registeration pcr ON o.hn = pcr.hn
+        LEFT OUTER JOIN patient p ON p.hn = o.hn
+    WHERE o.vstdate = CURDATE()
+		
+		AND (od.icd10 IN ("9221", "9222", "9223", "9224", "9225", "9226", "9227", "9241", "9231", "9232", "9233", "9359")
+			or o.vn in (select vn from ovstoprt where vn = o.vn and icd9cm in ("9221", "9222", "9223", "9224", "9225", "9226", "9227", "9241", "9231", "9232", "9233", "9359")))
+			
+) DAT 
+WHERE icd10_code IS NOT NULL;
